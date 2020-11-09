815216abcc2cb11b1e2779323643f93c079913e3
rarosales <rarosales@indracompany.com>
2020-11-09 10:16:10 -0500

    feat: validaciones login

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, TemplateRef } from '@angular/core';
 import { Router } from '@angular/router';
+
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
 import { LoginService } from './login.service';
 
@@ -12,17 +14,33 @@ export class LoginComponent {
   
   usuario: any = {};
   
+  modalRef: BsModalRef;
+  
+  @ViewChild('templateUsuarioNoEncontrado', { static: true }) public templateUsuarioNoEncontrado: TemplateRef<any>;
+  @ViewChild('templateUsuarioNoDatos', { static: true }) public templateUsuarioNoDatos: TemplateRef<any>;
+  
   constructor(
     private router: Router,
-    private loginService: LoginService) {}
+    private loginService: LoginService,
+    private modalService: BsModalService) {}
   
   clickIngresar() {
-    this.loginService.login(this.usuario).subscribe(
-      res => {
-        if(res.usuarioID) {
-          this.router.navigate(['tareas']);
+    if(!this.usuario.username || !this.usuario.password) {
+      this.openModal(this.templateUsuarioNoDatos, 'modal-sm');
+    } else {    
+      this.loginService.login(this.usuario).subscribe(
+        res => {
+          if(res.usuarioID) {
+            this.router.navigate(['tareas']);
+          } else {
+            this.openModal(this.templateUsuarioNoEncontrado, 'modal-sm');
+          }
         }
-      }
-    );
+      );
+    }
+  }
+  
+  openModal(template: TemplateRef<any>, tamanio?) {
+    this.modalRef = this.modalService.show(template, {class: tamanio});
   }
 }
